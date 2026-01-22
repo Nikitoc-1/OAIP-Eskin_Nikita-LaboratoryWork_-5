5314dbfeb3d0017531d9810babc214e96bacc553
esk <nik.eskin.2000@mail.ru>
2026-01-22 14:57:48 +0300

    Первый коммит

---
+++ EskinLaba5/Program.cs
@@ -0,0 +1,19 @@
+﻿
+using Laba5OAIP;
+using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.Design;
+using System.Runtime.ExceptionServices;
+namespace Hello
+{
+    class Program 
+    {
+        static void Main() {
+            Scope range = new Scope();
+            int a, b, c;
+            range.Read(out a, out b, out c);
+            string total = range.rangecheck(a, b, c);
+            range.Display(total);
+        }
+    }
+}
---
+++ EskinLaba5/Scope.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Laba5OAIP
+{
+    public class Scope
+    {
+
+        public int first;
+        public int second;
+        public int number;
+        public string resualt;
+
+        public void Read(out int first, out int second, out int number)
+        {
+            Console.WriteLine("Введите число(левую границу диапазона):");
+            first = int.Parse(Console.ReadLine());
+            Console.WriteLine("Введите число(правую границу диапазона):");
+            second = int.Parse(Console.ReadLine());
+            Console.WriteLine("Введите любое число:");
+            number = int.Parse(Console.ReadLine());
+        }
+
+        public void Display(string resualt)
+        {
+            Console.WriteLine(resualt);
+        }
+
+        public string rangecheck(int first, int second, int number)
+        {
+            if (number >= first && number < second){
+                return resualt = "Число принадлежит диапазону";
+            }
+            else{
+                return resualt = "Число не принадлежит диапазону";
+            }
+        }
+    }
+}
